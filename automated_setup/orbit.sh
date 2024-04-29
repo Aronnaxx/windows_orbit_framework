b2c1ff33248a2b56f24521d1af6aa0f3ed7c0823
Aronnaxx <79958715+Aronnaxx@users.noreply.github.com>
2024-04-28 18:32:45 -0700

    Removing old linux version

--- automated_setup/orbit.sh
+++ automated_setup/orbit.sh
@@ -17,7 +17,7 @@ tabs 4
 
 # get source directory
 export ORBIT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
+export ISAACSIM_PATH="/c/Users/$USERNAME/AppData/Local/ov/pkg/isaac_sim-*"
 #==
 # Helper functions
 #==
@@ -25,19 +25,10 @@ export ORBIT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 # extract the python from isaacsim
 extract_python_exe() {
     # Check if IsaacSim directory manually specified
-    # Note: for manually build isaacsim, this: _build/linux-x86_64/release
     if [ ! -z ${ISAACSIM_PATH} ];
     then
         # Use local build
         build_path=${ISAACSIM_PATH}
-    else
-        # Use TeamCity build
-        build_path=${ORBIT_PATH}/_isaac_sim
-    fi
-    # check if using conda
-    if ! [[ -z "${CONDA_PREFIX}" ]]; then
-        # use conda python
-        local python_exe=${CONDA_PREFIX}/bin/python
     else
         # use python from kit
         local python_exe=${build_path}/python.bat
@@ -54,14 +45,10 @@ extract_python_exe() {
 # extract the simulator exe from isaacsim
 extract_isaacsim_exe() {
     # Check if IsaacSim directory manually specified
-    # Note: for manually build isaacsim, this: _build/linux-x86_64/release
     if [ ! -z ${ISAACSIM_PATH} ];
     then
         # Use local build
         build_path=${ISAACSIM_PATH}
-    else
-        # Use TeamCity build
-        build_path=${ORBIT_PATH}/_isaac_sim
     fi
     # Isaac Sim executable to use
     local isaacsim_exe=${build_path}/isaac-sim.bat
@@ -86,90 +73,6 @@ install_orbit_extension() {
     fi
 }
 
-# setup anaconda environment for orbit
-setup_conda_env() {
-    # get environment name from input
-    local env_name=$1
-    # check conda is installed
-    if ! command -v conda &> /dev/null
-    then
-        echo "[ERROR] Conda could not be found. Please install conda and try again."
-        exit 1
-    fi
-    # Check if IsaacSim directory manually specified
-    # Note: for manually build isaacsim, this: _build/linux-x86_64/release
-    if [ ! -z ${ISAACSIM_PATH} ];
-    then
-        # Use local build
-        build_path=${ISAACSIM_PATH}
-    else
-        # Use TeamCity build
-        build_path=${ORBIT_PATH}/_isaac_sim
-    fi
-    # check if the environment exists
-    if { conda env list | grep -w ${env_name}; } >/dev/null 2>&1; then
-        echo -e "[INFO] Conda environment named '${env_name}' already exists."
-    else
-        echo -e "[INFO] Creating conda environment named '${env_name}'..."
-        conda env create --name ${env_name} -f ${build_path}/environment.yml
-    fi
-    # cache current paths for later
-    cache_pythonpath=$PYTHONPATH
-    cache_ld_library_path=$LD_LIBRARY_PATH
-    # clear any existing files
-    rm -f ${CONDA_PREFIX}/etc/conda/activate.d/setenv.sh
-    rm -f ${CONDA_PREFIX}/etc/conda/deactivate.d/unsetenv.sh
-    # activate the environment
-    source $(conda info --base)/etc/profile.d/conda.sh
-    conda activate ${env_name}
-    # setup directories to load isaac-sim variables
-    mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
-    mkdir -p ${CONDA_PREFIX}/etc/conda/deactivate.d
-    # add variables to environment during activation
-    local isaacsim_setup_conda_env_script=${ORBIT_PATH}/_isaac_sim/setup_conda_env.sh
-    printf '%s\n' '#!/usr/bin/env bash' '' \
-        '# for isaac-sim' \
-        'source '${isaacsim_setup_conda_env_script}'' \
-        '' \
-        '# for orbit' \
-        'alias orbit='${ORBIT_PATH}'/orbit.sh' \
-        '' \
-        '# show icon if not runninng headless' \
-        'export RESOURCE_NAME="IsaacSim"' \
-        '' > ${CONDA_PREFIX}/etc/conda/activate.d/setenv.sh
-    # reactivate the environment to load the variables
-    # needed because deactivate complains about orbit alias since it otherwise doesn't exist
-    conda activate ${env_name}
-    # remove variables from environment during deactivation
-    printf '%s\n' '#!/usr/bin/env bash' '' \
-        '# for orbit' \
-        'unalias orbit &>/dev/null' \
-        '' \
-        '# for isaac-sim' \
-        'unset CARB_APP_PATH' \
-        'unset EXP_PATH' \
-        'unset ISAAC_PATH' \
-        'unset RESOURCE_NAME' \
-        '' \
-        '# restore paths' \
-        'export PYTHONPATH='${cache_pythonpath}'' \
-        'export LD_LIBRARY_PATH='${cache_ld_library_path}'' \
-        '' > ${CONDA_PREFIX}/etc/conda/deactivate.d/unsetenv.sh
-    # install some extra dependencies
-    echo -e "[INFO] Installing extra dependencies (this might take a few minutes)..."
-    conda install -c conda-forge -y importlib_metadata &> /dev/null
-    # deactivate the environment
-    conda deactivate
-    # add information to the user about alias
-    echo -e "[INFO] Added 'orbit' alias to conda environment for 'orbit.sh' script."
-    echo -e "[INFO] Created conda environment named '${env_name}'.\n"
-    echo -e "\t\t1. To activate the environment, run:                conda activate ${env_name}"
-    echo -e "\t\t2. To install orbit extensions, run:                orbit -i"
-    echo -e "\t\t3. To install learning-related dependencies, run:   orbit -e"
-    echo -e "\t\t4. To perform formatting, run:                      orbit -f"
-    echo -e "\t\t5. To deactivate the environment, run:              conda deactivate"
-    echo -e "\n"
-}
 
 # update the vscode settings from template and isaac sim settings
 update_vscode_settings() {
@@ -188,22 +91,17 @@ update_vscode_settings() {
 
 # print the usage description
 print_help () {
-    echo -e "\nusage: $(basename "$0") [-h] [-i] [-e] [-f] [-p] [-s] [-o] [-v] [-d] [-c] -- Utility to manage extensions in Orbit."
+    echo -e "\nusage: $(basename "$0") [-h] [-i] [-e] [-p] [-s] [-t] [-v] -- Utility to manage extensions in Orbit."
     echo -e "\noptional arguments:"
     echo -e "\t-h, --help           Display the help content."
     echo -e "\t-i, --install        Install the extensions inside Orbit."
     echo -e "\t-e, --extra          Install extra dependencies such as the learning frameworks."
-    echo -e "\t-f, --format         Run pre-commit to format the code and check lints."
     echo -e "\t-p, --python         Run the python executable (python.sh) provided by Isaac Sim."
     echo -e "\t-s, --sim            Run the simulator executable (isaac-sim.sh) provided by Isaac Sim."
     echo -e "\t-t, --test           Run all python unittest tests."
-    echo -e "\t-o, --docker         Run the docker container helper script (docker/container.sh)."
     echo -e "\t-v, --vscode         Generate the VSCode settings file from template."
-    echo -e "\t-d, --docs           Build the documentation from source using sphinx."
-    echo -e "\t-c, --conda [NAME]   Create the conda environment for Orbit. Default name is 'orbit'."
     echo -e "\n" >&2
 }
-
 
 #==
 # Main
@@ -252,47 +150,6 @@ while [[ $# -gt 0 ]]; do
             ${python_exe} -m pip install -e "./source/extensions/omni.isaac.orbit_tasks[${framework_name}]"
             shift # past argument
             ;;
-        -c|--conda)
-            # use default name if not provided
-            if [ -z "$2" ]; then
-                echo "[INFO] Using default conda environment name: orbit"
-                conda_env_name="orbit"
-            else
-                echo "[INFO] Using conda environment name: $2"
-                conda_env_name=$2
-                shift # past argument
-            fi
-            # setup the conda environment for orbit
-            setup_conda_env ${conda_env_name}
-            shift # past argument
-            ;;
-        -f|--format)
-            # reset the python path to avoid conflicts with pre-commit
-            # this is needed because the pre-commit hooks are installed in a separate virtual environment
-            # and it uses the system python to run the hooks
-            if [ -n "${CONDA_DEFAULT_ENV}" ]; then
-                cache_pythonpath=${PYTHONPATH}
-                export PYTHONPATH=""
-            fi
-            # run the formatter over the repository
-            # check if pre-commit is installed
-            if ! command -v pre-commit &>/dev/null; then
-                echo "[INFO] Installing pre-commit..."
-                pip install pre-commit
-            fi
-            # always execute inside the Orbit directory
-            echo "[INFO] Formatting the repository..."
-            cd ${ORBIT_PATH}
-            pre-commit run --all-files
-            cd - > /dev/null
-            # set the python path back to the original value
-            if [ -n "${CONDA_DEFAULT_ENV}" ]; then
-                export PYTHONPATH=${cache_pythonpath}
-            fi
-            shift # past argument
-            # exit neatly
-            break
-            ;;
         -p|--python)
             # run the python provided by isaacsim
             python_exe=$(extract_python_exe)
@@ -319,37 +176,9 @@ while [[ $# -gt 0 ]]; do
             # exit neatly
             break
             ;;
-        -o|--docker)
-            # run the docker container helper script
-            docker_script=${ORBIT_PATH}/docker/container.sh
-            echo "[INFO] Running docker utility script from: ${docker_script}"
-            shift # past argument
-            bash ${docker_script} $@
-            # exit neatly
-            break
-            ;;
         -v|--vscode)
             # update the vscode settings
             update_vscode_settings
-            shift # past argument
-            # exit neatly
-            break
-            ;;
-        -d|--docs)
-            # build the documentation
-            echo "[INFO] Building documentation..."
-            # retrieve the python executable
-            python_exe=$(extract_python_exe)
-            # install pip packages
-            cd ${ORBIT_PATH}/docs
-            ${python_exe} -m pip install -r requirements.txt > /dev/null
-            # build the documentation
-            ${python_exe} -m sphinx -b html -d _build/doctrees . _build/html
-            # open the documentation
-            echo -e "[INFO] To open documentation on default browser, run:"
-            echo -e "\n\t\txdg-open $(pwd)/_build/html/index.html\n"
-            # exit neatly
-            cd - > /dev/null
             shift # past argument
             # exit neatly
             break
--- automated_setup/windows_automated_setup.ps1
+++ automated_setup/windows_automated_setup.ps1
@@ -1,55 +1,6 @@
-# Check if CMake is installed and in the PATH
-$cmakePath = Get-Command cmake -ErrorAction SilentlyContinue
-if ($null -eq $cmakePath) {
-    Write-Host "CMake is not found." -ForegroundColor Red
-    $userChoice = Read-Host "Do you want to install CMake automatically? (Y for Yes, N for No, download manually)"
-    if ($userChoice -eq 'Y') {
-        # Check if Chocolatey is installed
-        $chocoPath = Get-Command choco -ErrorAction SilentlyContinue
-        if ($null -eq $chocoPath) {
-            Write-Host "Chocolatey is not installed. Installing Chocolatey..." -ForegroundColor Yellow
-            # Install Chocolatey
-            Set-ExecutionPolicy Bypass -Scope Process -Force
-            iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-        }
-        # Install CMake using Chocolatey
-        Write-Host "Installing CMake using Chocolatey..." -ForegroundColor Yellow
-        choco install cmake -y
-    } elseif ($userChoice -eq 'N') {
-        Write-Host "Please download and install CMake from https://cmake.org/download/ then rerun this script." -ForegroundColor Yellow
-        exit
-    } else {
-        Write-Host "Invalid input. Exiting script." -ForegroundColor Red
-        exit
-    }
-}
-
 # Set Isaac Sim directory
 $currentUsername = $env:USERNAME
 $isaac_sim_dir = "C:\Users\$currentUsername\AppData\Local\ov\pkg\isaac_sim-*"
-
-# Determine the directories based on script location
-# Get the current script's directory
-$script_dir = Split-Path $script:MyInvocation.MyCommand.Path
-# Get the 'automated_setup' directory
-$automated_setup_dir = $script_dir
-# Get the parent directory of 'automated_setup', which will also be the parent for 'orbit'
-$parent_dir = Split-Path $automated_setup_dir
-# Define the path for the 'orbit' directory, which should be a sibling to 'automated_setup'
-$orbit_dir = Join-Path $parent_dir "orbit"
-
-# Clone Orbit if it doesn't exist and switch to the latest release
-if (-not (Test-Path $orbit_dir)) {
-    Write-Host "Orbit directory not found. Cloning..."
-    git clone https://github.com/NVIDIA-Omniverse/Orbit.git $orbit_dir
-    Push-Location $orbit_dir
-    git fetch --tags
-    $latest_tag = git describe --tags $(git rev-list --tags --max-count=1)
-    git checkout $latest_tag
-    Pop-Location
-} else {
-    Write-Host "Orbit directory found."
-}
 
 # Resolve the actual Isaac Sim directory by expanding the wildcard
 $isaac_sim_dir = Resolve-Path $isaac_sim_dir | Select-Object -First 1
@@ -65,13 +16,11 @@ Write-Host "Copying orbit.sh and .vscode folder to the Orbit directory..."
 Copy-Item "$parent_dir\automated_setup\orbit.sh" -Destination "$orbit_dir\orbit.sh" -Force
 Copy-Item "$parent_dir\automated_setup\.vscode" -Destination "$orbit_dir\" -Recurse -Force
 
-
 # Run Orbit commands via Git Bash
-Write-Host "Running Orbit setup commands..."
+Write-Host "Running Orbit setup commands..." 
 # Convert the Windows-style path to Unix-style for Git Bash
 $unixStyleOrbitDir = $orbit_dir -replace '^C:', '/c' -replace '\\', '/'
 
 Write-Host "Running Orbit setup commands..."
 $gitBashPath = "C:\Program Files\Git\bin\bash.exe" # Ensure this path is correct for the system
-& $gitBashPath -c "cd `"$unixStyleOrbitDir`"; ./orbit.sh -v; ./orbit.sh -i; ./orbit.sh -e rsl_rl; ./orbit.sh -p -m pip uninstall torch -y; ./orbit.sh -p -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118; ./orbit.sh -p source/standalone/demos/quadrupeds.py"
-# & $gitBashPath -c "cd `"$unixStyleOrbitDir`"; ./orbit.sh -v; ./orbit.sh -i; ./orbit.sh -e rsl_rl; ./orbit.sh -p -m pip uninstall torch -y; ./orbit.sh -p -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118; ./orbit.sh -p source/standalone/demos/quadrupeds.py"
+& $gitBashPath -c "cd `"$unixStyleOrbitDir`"; ./orbit.sh -v; ./orbit.sh -i; ./orbit.sh -e rsl_rl; ./orbit.sh -p -m pip uninstall torch -y; ./orbit.sh -p -m pip install torch torchvision torchaudio --index-url https://download.pytorch../orbit.sh -p source/standalone/demos/quadrupeds.py"
