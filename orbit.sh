84008b12792e95c8a2f3a79b99851154e4a9fbc3
Aronnaxx <79958715+Aronnaxx@users.noreply.github.com>
2024-04-29 17:44:11 -0700

    Easier setup, no admin needed

---
+++ orbit.sh
@@ -0,0 +1,160 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2022-2024, The ORBIT Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+#==
+# Configurations
+#==
+
+# Exits if error occurs
+set -e
+
+# Set tab-spaces
+tabs 4
+
+# get source directory
+export ORBIT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+#==
+# Helper functions
+#==
+
+# extract the python from isaacsim
+extract_python_exe() {
+    if [ ! -z ${ISAACSIM_PATH} ];
+    then
+        build_path=${ISAACSIM_PATH}
+    else
+        build_path=${ORBIT_PATH}/_isaac_sim
+    fi
+    if ! [[ -z "${CONDA_PREFIX}" ]]; then
+        local python_exe=${CONDA_PREFIX}/bin/python
+    else
+        local python_exe=${build_path}/python.bat
+    fi
+    if [ ! -f "${python_exe}" ]; then
+        echo "[ERROR] No python executable found at path: ${build_path}" >&2
+        exit 1
+    fi
+    echo ${python_exe}
+}
+
+# extract the simulator exe from isaacsim
+extract_isaacsim_exe() {
+    if [ ! -z ${ISAACSIM_PATH} ];
+    then
+        build_path=${ISAACSIM_PATH}
+    else
+        build_path=${ORBIT_PATH}/_isaac_sim
+    fi
+    local isaacsim_exe=${build_path}/isaac-sim.bat
+    if [ ! -f "${isaacsim_exe}" ]; then
+        echo "[ERROR] No isaac-sim executable found at path: ${build_path}" >&2
+        exit 1
+    fi
+    echo ${isaacsim_exe}
+}
+
+# check if input directory is a python extension and install the module
+install_orbit_extension() {
+    python_exe=$(extract_python_exe)
+    if [ -f "$1/setup.py" ];
+    then
+        echo -e "\t module: $1"
+        ${python_exe} -m pip install --editable $1
+    fi
+}
+
+# update the vscode settings from template and isaac sim settings
+update_vscode_settings() {
+    echo "[INFO] Setting up vscode settings..."
+    python_exe=$(extract_python_exe)
+    setup_vscode_script="${ORBIT_PATH}/.vscode/tools/setup_vscode.py"
+    if [ -f "${setup_vscode_script}" ]; then
+        ${python_exe} "${setup_vscode_script}"
+    else
+        echo "[WARNING] setup_vscode.py not found. Aborting vscode settings setup."
+    fi
+}
+
+# print the usage description
+print_help () {
+    echo -e "\nusage: $(basename "$0") [-h] [-i] [-e] [-p] [-s] [-v] -- Utility to manage extensions in Orbit."
+    echo -e "\noptional arguments:"
+    echo -e "\t-h, --help           Display the help content."
+    echo -e "\t-i, --install        Install the extensions inside Orbit."
+    echo -e "\t-e, --extra          Install extra dependencies such as the learning frameworks."
+    echo -e "\t-p, --python         Run the python executable (python.sh) provided by Isaac Sim."
+    echo -e "\t-s, --sim            Run the simulator executable (isaac-sim.sh) provided by Isaac Sim."
+    echo -e "\t-v, --vscode         Generate the VSCode settings file from template."
+    echo -e "\n" >&2
+}
+
+#==
+# Main
+#==
+
+if [ -z "$*" ]; then
+    echo "[Error] No arguments provided." >&2;
+    print_help
+    exit 1
+fi
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -i|--install)
+            echo "[INFO] Installing extensions inside orbit repository..."
+            export -f extract_python_exe
+            export -f install_orbit_extension
+            find -L "${ORBIT_PATH}/source/extensions" -mindepth 1 -maxdepth 1 -type d -exec bash -c 'install_orbit_extension "{}"' \;
+            unset install_orbit_extension
+            update_vscode_settings
+            shift
+            ;;
+        -e|--extra)
+            echo "[INFO] Installing extra requirements such as learning frameworks..."
+            python_exe=$(extract_python_exe)
+            if [ -z "$2" ]; then
+                echo "[INFO] Installing all rl-frameworks..."
+                framework_name="all"
+            else
+                echo "[INFO] Installing rl-framework: $2"
+                framework_name=$2
+                shift # past argument
+            fi
+            ${python_exe} -m pip install -e "./source/extensions/omni.isaac.orbit_tasks[${framework_name}]"
+            shift # past argument
+            ;;
+        -p|--python)
+            python_exe=$(extract_python_exe)
+            echo "[INFO] Using python from: ${python_exe}"
+            shift # past argument
+            ${python_exe} $@
+            break
+            ;;
+        -s|--sim)
+            isaacsim_exe=$(extract_isaacsim_exe)
+            echo "[INFO] Running isaac-sim from: ${isaacsim_exe}"
+            shift # past argument
+            ${isaacsim_exe} --ext-folder ${ORBIT_PATH}/source/extensions $@
+            break
+            ;;
+        -v|--vscode)
+            update_vscode_settings
+            shift # past argument
+            break
+            ;;
+        -h|--help)
+            print_help
+            exit 1
+            ;;
+        *) # unknown option
+            echo "[Error] Invalid argument provided: $1"
+            print_help
+            exit 1
+            ;;
+    esac
+done
--- windows_automated_setup.sh
+++ windows_automated_setup.sh
@@ -10,8 +10,7 @@ fi
 # ------------ SETUP OF ORBIT ------------
 
 # Set the ORBIT_PATH environment variable
-ORBIT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/dependencies/orbit" &> /dev/null && pwd )"
-export ORBIT_PATH
+export ORBIT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/dependencies/orbit" &> /dev/null && pwd )"
 echo "ORBIT_PATH: $ORBIT_PATH"
 
 # Check if ORBIT_PATH is already in .bashrc
@@ -19,30 +18,11 @@ if ! grep -q "export ORBIT_PATH=" ~/.bashrc; then
     echo "export ORBIT_PATH=\"$ORBIT_PATH\"" >> ~/.bashrc
 fi
 
-# Define the ORBIT_DIR variable
-ORBIT_DIR="$ORBIT_PATH"
-export ORBIT_DIR
-echo "ORBIT_DIR: $ORBIT_DIR"
-
-# Check if ORBIT_DIR is already in .bashrc
-if ! grep -q "export ORBIT_DIR=" ~/.bashrc; then
-    echo "export ORBIT_DIR=\"$ORBIT_DIR\"" >> ~/.bashrc
-fi
-
-# Add a function to run orbit.sh from $ORBIT_DIR
-if ! grep -q "orbit.sh()" ~/.bashrc; then
-    # Append orbit.sh function to .bashrc
-    echo 'orbit.sh() {
-    $ORBIT_DIR/orbit.sh "$@"
-}' >> ~/.bashrc
-fi
-
 
 # ---------- SETUP OF ISAAC SIM ----------
 
 # Set the ISAACSIM_PATH environment variable
-ISAACSIM_PATH="/c/Users/$USERNAME/AppData/Local/ov/pkg/isaac_sim-2023.1.1"
-export ISAACSIM_PATH
+export ISAACSIM_PATH="/c/Users/$USERNAME/AppData/Local/ov/pkg/isaac_sim-2023.1.1"
 echo "ISAACSIM_PATH: $ISAACSIM_PATH"
 
 # Check if ISAACSIM_PATH is already in .bashrc
@@ -51,8 +31,7 @@ if ! grep -q "export ISAACSIM_PATH=" ~/.bashrc; then
 fi
 
 # Set the ISAACSIM_PYTHON_EXE environment variable
-ISAACSIM_PYTHON_EXE="${ISAACSIM_PATH}/python.bat"
-export ISAACSIM_PYTHON_EXE
+export ISAACSIM_PYTHON_EXE="${ISAACSIM_PATH}/python.bat"
 echo "ISAACSIM_PYTHON_EXE: $ISAACSIM_PYTHON_EXE"
 
 # Check if ISAACSIM_PYTHON_EXE is already in .bashrc
@@ -60,22 +39,8 @@ if ! grep -q "export ISAACSIM_PYTHON_EXE=" ~/.bashrc; then
     echo "export ISAACSIM_PYTHON_EXE=\"$ISAACSIM_PYTHON_EXE\"" >> ~/.bashrc
 fi
 
-# Check if python.sh function is already defined in .bashrc
-if ! grep -q "python.sh()" ~/.bashrc; then
-    # Append python.sh function to .bashrc
-    echo 'python.sh() {
-    python.bat "$@"
-}' >> ~/.bashrc
-fi
 
-# Check if isaac-sim.sh function is already defined in .bashrc
-if ! grep -q "isaac-sim.sh()" ~/.bashrc; then
-    # Append isaac-sim.sh function to .bashrc
-    echo 'isaac-sim.sh() {
-    isaac-sim.bat "$@"
-}' >> ~/.bashrc
-fi
-
+# Source the .bashrc file and print it out
 source ~/.bashrc
 
 echo ""
@@ -84,15 +49,17 @@ echo ""
 
 cat ~/.bashrc
 
-
+# Confirm with the user that the paths are correct
 read -p "Press [Enter] key to start the rest of the setup, as long as these look correct..."
 
 
-# Copy the .vscode directory to dependencies/orbit
+# Copy the .vscode directory and orbit.sh to dependencies/orbit
 cp -r .vscode "dependencies/orbit/.vscode"
 
+cp orbit.sh "dependencies/orbit/orbit.sh"
 
 # checks that python path is set correctly
 ${ISAACSIM_PYTHON_EXE} -c "print('Isaac Sim configuration is now complete.')"
+
 
 echo "Setup complete!"
